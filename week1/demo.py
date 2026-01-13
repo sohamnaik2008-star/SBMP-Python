4783a0e83c4bd24fadfd98752b5d11592379bfe1
sohamnaik2008-star <sohamnaik2008@gmail.com>
2026-01-13 13:48:19 +0530

    Update demo.py

--- week1/demo.py
+++ week1/demo.py
@@ -1,1 +1,17 @@
-print ("Hi Hello")
+def factorial(n):
+    if n < 0:
+        print("Factorial is not defined for negative numbers")
+
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+def main():
+    num = int(input("Enter a non-negative integer: "))
+    print(factorial(num))
+
+
+if __name__ == "__main__":
+    main()
